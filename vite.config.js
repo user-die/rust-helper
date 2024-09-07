8b3d96581b285c355e79c72daac4c20b3a06685f
userdie <vadim.vadimov123@mail.ru>
2024-09-08 02:53:51 +0500

    update manifest

--- vite.config.js
+++ vite.config.js
@@ -11,35 +11,32 @@ export default defineConfig({
       registerType: "autoUpdate",
       injectRegister: "auto",
 
-      pwaAssets: {
-        disabled: false,
-        config: true,
-      },
-
-      icons: [
-        {
-          src: "icon64.png",
-          type: "image/png",
-          sizes: "64x64",
-        },
-        {
-          src: "icon192.png",
-          type: "image/png",
-          sizes: "192x192",
-        },
-        {
-          src: "icon512.png",
-          type: "image/png",
-          sizes: "512x512",
-        },
-      ],
-
       manifest: {
         name: "rust-helper",
         short_name: "rust-helper",
         description: "Progressive Web App with Offline Support",
         theme_color: "#16181c",
         background_color: "#16181c",
+        display_override: ["fullscreen", "minimal-ui"],
+        display: "standalone",
+        id: "/",
+        icons: [
+          {
+            src: "icon64.png",
+            type: "image/png",
+            sizes: "64x64",
+          },
+          {
+            src: "icon192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "icon512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
         screenshots: [
           {
             src: "screenshot1.png",
